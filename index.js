f911fcb8b49cd91502ec97f6c77af6c7d39f28a1
Hakim <jakirulislamhakim@gmail.com>
2023-12-31 03:21:40 +0600

    added our Shop page

--- index.js
+++ index.js
@@ -8,10 +8,8 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
-
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  `mongodb+srv://${process.env.db_user}:${process.env.db_pass}@cluster0.5prtsfh.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.db_user}:${process.env.db_pass}@cluster0.5prtsfh.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,6 +24,22 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const menuCollection = client.db("Bistro_Boss").collection("menu");
+    const reviewsCollection = client.db("Bistro_Boss").collection("reviews");
+
+    // all menu item
+    app.use("/api/v1/menu_item", async (req, res) => {
+      const result = await menuCollection.find().toArray();
+      res.send(result);
+    });
+
+    // client review
+    app.use("/api/v1/client_review", async (req, res) => {
+      const result = await reviewsCollection.find().toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -33,7 +47,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);

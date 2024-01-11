f330e5208d5af555ce011776f4c9cfb7b12de313
Hakim <jakirulislamhakim@gmail.com>
2024-01-11 17:22:42 +0600

    do some basic curd oparation

--- index.js
+++ index.js
@@ -8,10 +8,8 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
-
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  `mongodb+srv://${process.env.db_user}:${process.env.db_pass}@cluster0.5prtsfh.mongodb.net/?retryWrites=true&w=majority`;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.db_user}:${process.env.db_pass}@cluster0.5prtsfh.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -25,7 +23,97 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+
+    const menuCollection = client.db("Bistro_Boss").collection("menu");
+    const cartsCollection = client.db("Bistro_Boss").collection("carts");
+    const reviewsCollection = client.db("Bistro_Boss").collection("reviews");
+    const usersInfoCollection = client
+      .db("Bistro_Boss")
+      .collection("usersInfo");
+
+    // user related api
+
+    // store user data
+    app.post("/api/v1/user/userInfo", async (req, res) => {
+      const info = req.body;
+      // check user isExist
+      const query = { email: req.body.email };
+      const isExist = await usersInfoCollection.findOne(query);
+      if (isExist) {
+        return res.send({ message: "user already exist", insertedId: null });
+      }
+      const result = await usersInfoCollection.insertOne(info);
+      res.send(result);
+    });
+
+    // get user data
+    app.get("/api/v1/user/userInfo", async (req, res) => {
+      const result = await usersInfoCollection.find().toArray();
+      res.send(result);
+    });
+
+    // delete user by admin
+    app.delete("/api/v1/admin/removeUser/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await usersInfoCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // make admin
+    app.patch("/api/v1/makeAdmin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateRole = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await usersInfoCollection.updateOne(filter, updateRole);
+      res.send(result);
+    });
+
+    // menu related api
+
+    // all menu item
+    app.get("/api/v1/menu_item", async (req, res) => {
+      const result = await menuCollection.find().toArray();
+      res.send(result);
+    });
+
+    // add to cart
+    app.post("/api/v1/user/addToCart", async (req, res) => {
+      const items = req.body;
+      const result = await cartsCollection.insertOne(items);
+      res.send(result);
+    });
+
+    // get user specific addToCart data
+    app.get("/api/v1/user/get_carts_data", async (req, res) => {
+      let query = {}; //when admin use
+      if (req.query.email) {
+        query.user = req.query.email;
+      }
+      // const query = { user: req.query.email }; //only user use
+      const result = await cartsCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    // delete cart
+    app.delete("/api/v1/user/deleteCart/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartsCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // client review
+    app.get("/api/v1/client_review", async (req, res) => {
+      const result = await reviewsCollection.find().toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -33,7 +121,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
